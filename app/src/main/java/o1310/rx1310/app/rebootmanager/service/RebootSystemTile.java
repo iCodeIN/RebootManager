15f76d9e1af3c2d957a950cdfcc6736846da3bfc
rx1310 <rx1310@inbox.ru>
2020-04-05 09:07:26 +0300

    Доработал код QuickTile

--- app/src/main/java/o1310/rx1310/app/rebootmanager/service/RebootSystemTile.java
+++ app/src/main/java/o1310/rx1310/app/rebootmanager/service/RebootSystemTile.java
@@ -6,31 +6,49 @@
 
 package o1310.rx1310.app.rebootmanager.service;
 
-
+import android.graphics.drawable.Icon;
 import android.os.Build;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.support.annotation.RequiresApi;
 import eu.chainfire.libsuperuser.Shell;
-import o1310.rx1310.app.rebootmanager.dialog.RootNotAviableDialog;
+import o1310.rx1310.app.rebootmanager.R;
 import o1310.rx1310.app.rebootmanager.RebootManager;
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class RebootSystemTile extends TileService {
 
 	@Override
-	public void onClick() {
+	public void onStartListening() {
 		
-		if (!Shell.SU.available()) {
-			showError();
+		Tile t = getQsTile();
+		
+		t.setIcon(Icon.createWithResource(this, R.drawable.ic_reboot));
+		t.setLabel(getString(R.string.tile_reboot_system));
+		
+		if (Shell.SU.available()) {
+			t.setState(Tile.STATE_ACTIVE);
 		} else {
-			Shell.SU.run(RebootManager.CMD_REBOOT_SYS);
+			t.setState(Tile.STATE_UNAVAILABLE);
 		}
+		
+		t.updateTile();
 		
 	}
 	
-	private void showError() {
-		showDialog(RootNotAviableDialog.getDialog(this));
+	// обработка нажатия
+	@Override
+	public void onClick() {
+		
+		// проверка root
+		if (!Shell.SU.available()) {
+			// сообщение об ошибке
+			RebootManager.showToast(getString(R.string.msg_root_not_aviable), this);
+		} else {
+			// выполнение команды
+			Shell.SU.run(RebootManager.CMD_REBOOT_SYS);
+		}
+		
 	}
 	
 }
