4d1379c4cc27f0762dd264321fc4660a38da35aa
rx1310 <rx1310@inbox.ru>
2020-05-28 17:03:23 +0300

    Удалил из основной ветки дополнительный функционал

--- app/src/main/java/o1310/rx1310/app/rebootmanager/RebootManager.java
+++ app/src/main/java/o1310/rx1310/app/rebootmanager/RebootManager.java
@@ -15,18 +15,8 @@ import android.widget.Toast;
 
 public class RebootManager {
 	
-	public static String CMD_SHUTDOWN = "su -c svc power shutdown"; // команда для выключения устройства
-	public static String CMD_SHUTDOWN_V2 = "su -c reboot -p"; // команда для выключения устройства (v2)
 	public static String CMD_REBOOT_RECOVERY = "su -c svc power reboot recovery"; // команда для перехода в recovery
-	public static String CMD_REBOOT_RECOVERY_V2 = "su -c reboot recovery"; // команда для перехода в recovery (v2)
 	public static String CMD_REBOOT_BOOTLOADER = "su -c svc power reboot bootloader"; // команда для перехода в bootloader
-	public static String CMD_REBOOT_BOOTLOADER_V2 = "su -c reboot bootloader"; // команда для перехода в bootloader (v2)
-	public static String CMD_REBOOT_DOWNLOAD = "su reboot download"; // команда для перехода в download mode
-	public static String CMD_REBOOT_SYS = "su -c svc power reboot"; // команда для перезапуска системы
-	public static String CMD_REBOOT_SYS_V2 = "su -c reboot system"; // команда для перезапуска системы (v2)
-	public static String CMD_REBOOT_SYS_SOFT = "setprop ctl.restart zygote"; // команда для soft-reboot
-	public static String CMD_SAFE_MODE = "setprop persist.sys.safemode 1 && setprop ctl.restart zygote"; // команда для перехода в безопасный режим
-	//public static String TEST_CMD_FAKE_BATTERY_LOW = "am broadcast -a android.intent.action.BATTERY_LOW";
 	
 	// версия приложения
 	public static String appVersion(Context c) {
